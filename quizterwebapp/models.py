688ffb9a1a8e81b6bf733efd8fbaa9209f9fe325
August Aublet <august.aublet@gmail.com>
2022-10-13 15:49:39 +0200

    lagt till lista för categorier. Ändrat highscore till score och ändrat databasNamn

--- quizterwebapp/models.py
+++ quizterwebapp/models.py
@@ -1,8 +1,9 @@
+from lib2to3.pgen2.token import OP
 import uuid
 from typing import Optional
 from pydantic import BaseModel, Field
 
-class Highscore(BaseModel):
+class Score(BaseModel):
     id: str = Field(default_factory=uuid.uuid4, alias="_id")
     playerName: str = Field(...)
     difficulty: str = Field(...)
@@ -10,25 +11,27 @@ class Highscore(BaseModel):
     score: int = Field(...)
     longestStreak: int = Field(...)
     numberOfQuestions: int = Field(...)
+    categories: list[str] = []
 
     class Config:
         allow_population_by_field_name = True
         schema_extra = {
             "example": {
-                "_id": "066de609-b04a-4b30-b46c-32537c7f1f6e",
                 "playerName": "Kalle Anka",
                 "difficulty": "medium",
                 "score": 12,
                 "longestStreak": 4,
                 "numberOfQuestions": 15,
+                "categories": ["Art & literature", "Sports"]
             }
         }
-class HighscoreUpdate(BaseModel):
+class ScoreUpdate(BaseModel):
     name: Optional[str]
     difficulty: Optional[str]
     score: Optional[int]
     longestStreak: Optional[int]
     numberOfQuestions: Optional[int]
+    categories: Optional[list]
 
     class Config:
         schema_extra = {
@@ -38,19 +41,11 @@ class HighscoreUpdate(BaseModel):
                 "score": 12,
                 "longestStreak": 4,
                 "numberOfQuestions": 15,
-            }
-        }
-class HighscoreHighest(BaseModel):
-    difficulty: Optional[str]
-    
-    class Config:
-        schema_extra = {
-            "example":{
-                "difficulty":"medium",
+                "categories": ["Art & literature", "Sports"]
             }
         }
 
-class HighscoreDeleteAll(BaseModel):
+class ScoreDeleteAll(BaseModel):
     confirm: bool
     class Config:
         schema_extra = {
--- quizterwebapp/routes.py
+++ quizterwebapp/routes.py
@@ -3,24 +3,26 @@ from fastapi import APIRouter, Body, Request, Response, HTTPException, status
 from fastapi.encoders import jsonable_encoder
 from typing import List
 
-from models import Highscore, HighscoreUpdate, HighscoreDeleteAll, HighscoreHighest
+from models import Score, ScoreDeleteAll
+
+DATABASE_NAME = "Highscore"
 
 router = APIRouter()
 
-@router.post("/", response_description = "Add new highscore", status_code=status.HTTP_201_CREATED, response_model=Highscore)
-def add_highscore(request: Request, newHighScore: Highscore = Body(...)):
-    newHighscore = jsonable_encoder(newHighScore)
-    new_highscore= request.app.database["highscores"].insert_one(newHighscore)
-    added_highscore = request.app.database["highscores"].find_one(
-        {"_id":new_highscore.inserted_id}
+@router.post("/", response_description = "Add new Score", status_code=status.HTTP_201_CREATED, response_model=Score)
+def add_Score(request: Request, response: Score = Body(...)):
+    newScore = jsonable_encoder(response)
+    new_score= request.app.database[DATABASE_NAME].insert_one(newScore)
+    added_score = request.app.database[DATABASE_NAME].find_one(
+        {"_id":new_score.inserted_id}
     )
-    return added_highscore
+    return added_score
 
 @router.get("/highscore", response_description="Get the highest 10",)
 def get_top_ten(request:Request):
-    easy = list(request.app.database["highscores"].find({"difficulty":"easy"}).limit(10).sort("score",-1))
-    medium = list(request.app.database["highscores"].find({"difficulty":"medium"}).limit(10).sort("score",-1))
-    hard = list(request.app.database["highscores"].find({"difficulty":"hard"}).limit(10).sort("score",-1))
+    easy = list(request.app.database[DATABASE_NAME].find({"difficulty":"easy"}).limit(10).sort("score",-1))
+    medium = list(request.app.database[DATABASE_NAME].find({"difficulty":"medium"}).limit(10).sort("score",-1))
+    hard = list(request.app.database[DATABASE_NAME].find({"difficulty":"hard"}).limit(10).sort("score",-1))
     respons = {
         "easy":easy,
         "medium":medium,
@@ -28,27 +30,27 @@ def get_top_ten(request:Request):
     }
     return respons
 
+@router.get("/", response_description="List all Scores", response_model=List[Score])
+def list_all_Scores(request:Request):
+    scores = list(request.app.database[DATABASE_NAME].find(limit=100))
+    return scores
 
 
-@router.get("/", response_description="List all Highscores", response_model=List[Highscore])
-def list_all_highscores(request:Request):
-    highscores = list(request.app.database["highscores"].find(limit=100))
-    return highscores
 
-@router.delete("/{id}", response_description="Delete a highscore")
-def delete_highsore(id: str, request:Request, response:Response):
-    delete_result = request.app.database["highscores"].delete_one({"_id":id})
+@router.delete("/{id}", response_description="Delete a Score")
+def delete_Sore(id: str, request:Request, response:Response):
+    delete_result = request.app.database[DATABASE_NAME].delete_one({"_id":id})
 
     if delete_result.deleted_count == 1:
         response.status_code = status.HTTP_204_NO_CONTENT
         return response
     
-    raise HTTPException(staus_code=status.HRRP_404_NOT_FOUND, detail=f"Highscore with {id} not found")
+    raise HTTPException(staus_code=status.HRRP_404_NOT_FOUND, detail=f"Score with {id} not found")
 
-@router.delete("/all", response_description="Delete all Highscores")
-def delete_all_highscores(request:Request, respons: HighscoreDeleteAll = Body(...)):
+@router.delete("/all", response_description="Delete all Scores")
+def delete_all_Scores(request:Request, respons: ScoreDeleteAll = Body(...)):
     if respons.confirm:
-        delete_respons = request.app.database["highscores"].delete_many({"score":{"$gt":0}})
+        delete_respons = request.app.database[DATABASE_NAME].delete_many({"score":{"$gt":0}})
         return f"{delete_respons.deleted_count} documents deletet."
     
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"You must confirm delete.")

96ce446213b1630c65b64425bccfa037568e54c8
August Aublet <august.aublet@gmail.com>
2022-10-13 22:28:24 +0200

    bara lagt till WSGIMIddleware

--- quizterwebapp/main.py
+++ quizterwebapp/main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.wsgi import WSGIMiddleware
 from dotenv import dotenv_values
 from pymongo import MongoClient
 from routes import router as highscore_router
+from flask import Flask
 
 config = dotenv_values(".env")
 
+flask_app = Flask(__name__)
+
 app = FastAPI()
+
 
 @app.on_event("startup")
 def startup_db_client():
@@ -18,3 +23,5 @@ def shutdown_db_client():
     app.mongodb_client.close()
 
 app.include_router(highscore_router, tags=["scores"], prefix="/scores")
+
+app.mount("/scores", WSGIMiddleware(flask_app))
